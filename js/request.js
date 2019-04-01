f0b97207af35a222e8440f030337464a6e6c1a21
Nikita Nagalsky <nagalsky.nikita@gmail.com>
2019-04-01 18:36:56 +0300

    commit

--- js/request.js
+++ js/request.js
@@ -1,45 +1,29 @@
 // Request to server
-;(function() {
-  var method = 'GET'
-  var url = './data/data.json'
-  var preloader = document.querySelector('#preloader')
-  var productsList = document.querySelector('#products-list')
+;(() => {
+  const method = 'GET'
+  const url = './data/products.json'
+  const preloader = document.querySelector('#preloader')
+  const productsList = document.querySelector('#products-list')
 
-  function ajax(url, method) {
-    return fetch(url).then(function(response) {
-      return response.json()
-    })
-  }
+  const ajax = (url, method) => fetch(url).then(response => response.json())
 
-  setTimeout(function() {
-    ajax(url, method).then(function(data) {
-      //console.log(data)
+  setTimeout(() => {
+    ajax(url, method).then(data => {
       preloader ? preloader.classList.remove('preloader--is-active') : preloader
       var productItem = data
-        .map(function(productItem) {
-          return (
-            '<li class="products-list__item">' +
-            '<img src="' +
-            productItem.avatar_url +
-            '" class="products-list__image" />' +
-            '<p>' +
-            productItem.title +
-            '</p>' +
-            '<p>' +
-            '<span>' +
-            '$' +
-            '</span>' +
-            productItem.price +
-            '</p>' +
-            '<button type="button" class="products-list__btn">' +
-            'Add to cart' +
-            '</button>' +
-            '</li>'
-          )
+        .map(productItem => {
+          return `
+            <li class="products-list__item">
+              <img src="${productItem.avatar_url}" alt="${productItem.title}" class="products-list__image">
+              <p>${productItem.title}</p>
+              <p><span>$</span>${productItem.price}</p>
+              <button type="button" class="products-list__btn" data-id=${productItem.id}>Add to cart</button>
+            </li>
+            `
         })
         .join('')
 
       productsList.innerHTML = productItem
     })
-  }, 2500)
+  }, 500)
 })()
